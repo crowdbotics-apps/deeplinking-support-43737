300976f3d5d4cc49d90de69131e8d0913e6d8701
Crowdbotics <team@crowdbotics.com>
2023-10-03 17:39:53 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "deeplinking-support-43737",
+  url: "https://deeplinking-support-43737.botics.co",
+  api: "https://deeplinking-support-43737.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
